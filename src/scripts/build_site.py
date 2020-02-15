b5bf86c0a930f04840dd511a444254de7e77f858
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2020-02-14 19:47:52 -0800

    WIP, before switching to Jinja2

--- src/scripts/build_site.py
+++ src/scripts/build_site.py
@@ -1,8 +1,46 @@
-import glob
 import html
 import os
+import re
+from collections import OrderedDict
 from configparser import RawConfigParser
-from json import loads
+from glob import glob
+from time import strptime
+from typing import Dict, List
+
+
+def read_info(filepath):
+    with open(filepath) as f:
+        info_string = f.read()
+    if not re.search("^\[.*?\]", info_string):
+        print(filepath + " has no section")
+        info_string = "[nosection]\n" + info_string
+    info = RawConfigParser()
+    info.optionxform = str
+    info.read_string(info_string)
+    return info
+
+
+def get_comic_list(date_format: str) -> "OrderedDict[str, RawConfigParser]":
+    comic_info_dicts = {}
+    for comic_name in glob("your_content/comics/*"):
+        comic_info_dicts[os.path.basename(comic_name)] = read_info("{}/info.ini".format(comic_name))
+
+    print(comic_info_dicts.keys())
+    comic_info_dicts = OrderedDict(sorted(
+        comic_info_dicts.items(),
+        key=lambda t: (strptime(t[1].get("nosection", "Post date"), date_format), t[0])
+    ))
+    print(comic_info_dicts.keys())
+    return comic_info_dicts
+
+
+def get_ids(comic_list: List, index):
+    return {
+        "first_id": comic_list[0],
+        "previous_id": comic_list[0] if index == 0 else comic_list[index - 1],
+        "next_id": comic_list[-1] if index == (len(comic_list) - 1) else comic_list[index + 1],
+        "last_id": comic_list[-1]
+    }
 
 
 def build_links_bar(comic_info: RawConfigParser):
@@ -14,34 +52,49 @@ def build_links_bar(comic_info: RawConfigParser):
     return "&nbsp;&nbsp;|&nbsp;&nbsp;".join(link_list)
 
 
-with open("src/templates/comic.html") as f:
-    comic_template = f.read()
+def tagged_link(tag):
+    return f'<a href="tagged.html?={}"'
 
-comic_info = RawConfigParser()
-comic_info.read("your_content/comic_info.ini")
 
-with open("your_content/comics/197/info.json") as f:
-    info_json = loads(f.read())
+def create_comic(comic_info: RawConfigParser, page, page_info, first_id, previous_id, next_id, last_id):
+    print("Building page {}...".format(page))
+    with open("your_content/comics/{}/post.html".format(page), "rb") as f:
+        post_html = f.read().decode("utf-8")
+    return {
+        "page_title": page_info.get("nosection", "Title") + " - " + comic_info.get("Comic Settings", "Comic name"),
+        "links_bar": build_links_bar(comic_info),
+        "comic_path": "../your_content/comics/{}/{}".format(page, page_info.get("nosection", "Filename")),
+        "alt_text": html.escape(page_info.get("nosection", "Alt text")),
+        "first_id": first_id,
+        "previous_id": previous_id,
+        "next_id": next_id,
+        "last_id": last_id,
+        "comic_title": page_info.get("nosection", "Title"),
+        "post_date": page_info.get("nosection", "Post date"),
+        "tags": page_info.get("nosection", "Tags"),
+        "post_html": post_html
+    }
 
-with open("your_content/comics/197/post.html", "rb") as f:
-    post_html = f.read().decode("utf-8")
 
-comic_dict = {
-    "page_title": info_json["title"] + " - " + comic_info.get("Comic Settings", "Site Name"),
-    "links_bar": build_links_bar(comic_info),
-    "comic_path": "../your_content/comics/197/" + info_json["filename"],
-    "alt_text": html.escape(info_json["alt_text"]),
-    "first_id": "197",
-    "previous_id": "197",
-    "next_id": "199",
-    "last_id": "200",
-    "comic_title": info_json["title"],
-    "post_date": info_json["post_date"],
-    "tags": ", ".join(info_json["tags"]),
-    "post_html": post_html
-}
+def write_comics(comics: Dict[str, Dict]):
+    with open("src/templates/comic.html") as f:
+        comic_template = f.read()
+    for page_name, comic_dict in comics.items():
+        with open("comic/{}.html".format(page_name), "wb") as f:
+            f.write(bytes(comic_template.format(**comic_dict), "utf-8"))
 
-os.makedirs("comic", exist_ok=True)
 
-with open("comic/197.html", "wb") as f:
-    f.write(bytes(comic_template.format(**comic_dict), "utf-8"))
+def main():
+    os.makedirs("comic", exist_ok=True)
+    comic_info = read_info("your_content/comic_info.ini")
+    comic_info_dicts = get_comic_list(comic_info.get("Comic Settings", "Date format"))
+    comic_output_dict = {}
+    comic_list = [os.path.basename(comic_name) for comic_name in comic_info_dicts.keys()]
+    for i, k in enumerate(comic_list):
+        ids = get_ids(comic_list, i)
+        comic_output_dict[k] = create_comic(comic_info, k, comic_info_dicts[k], **ids)
+    write_comics(comic_output_dict)
+
+
+if __name__ == "__main__":
+    main()
